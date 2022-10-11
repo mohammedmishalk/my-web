d523e371c03359eca7f6e6ae52948f0cb5b6d0cb
mohammedmishalk <mishalnunu@gmail.com>
2022-10-11 15:37:10 +0530

    form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -284,13 +284,11 @@ const firebaseConfig = {
   
     saveMassage(name,eamil,phone,massage);
 
-    document.querySelector(".alert").style.display = "block";
+   
 
 
      //   remove the alert
-  setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-  }, 3000);
+  
 
    //   reset the form
    document.getElementById("contactForm").reset();
